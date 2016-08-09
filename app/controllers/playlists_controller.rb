bbed151b4b80dc27008e81747bd1379eca28951b
Julia Najafi <Julia.Najafi@gmail.com>
2016-08-09 11:49:33 -0400

    added render json for playlists and words

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,5 +1,46 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :update, :destroy]
+
+  
   def index
-    # @playlists = Playlist.all
+    @playlists = Playlist.all
+
+    render json: @playlists
   end
+
+  def show
+    render json: @playlist
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    if @playlist.save
+      render json: @playlist, status: :created, location: @playlist
+    else
+      render json: @playlist.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @playlist.update(playlist_params)
+      render json: @playlist
+    else
+      render json: @playlist.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @playlist.destroy
+  end
+
+  private
+    def set_playlist
+      @playlist = Playlist.find(params[:id])
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:category)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :playlists
+  root :to => redirect('/playlists.json')
+  resources :playlists do
+    resources :words
+  end
 
 end
